dba3f3c6a33254d2c46d016e8818530f6d75cd6c
Duyen Ho <duyenho@gmail.com>
2016-10-14 13:07:09 +1100

    fixed style game still needs fine tuning

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,7 @@
+var squares = document.getElementsByClassName('square');
+var message = document.getElementById('message');
+var button = document.getElementById('button');
+
 var player1 = 'X';
 var player2 = 'O';
 var currentPlayer = player1;
@@ -11,28 +15,25 @@ var board = [['E', 'E', 'E'],
             ['E', 'E', 'E']];
 
 var updateGame = function() {
-  if ( isEmpty() ) {
+
+  if (isEmpty()) {
     recordMove();
     changeDOM();
-    if ( checkWin() ) {
+    if (checkWin()) {
       declareWinner();
       resetGame();
     } else {
       changePlayer();
     };
   };
+
 };
 
-// when i move this line down with the other DOM elements, makeEventListeners won't run ?
-var squares = document.getElementsByClassName('square');
-
-var makeEventListeners = function() {
+function makeEventListeners() {
   for (var i = 0; i < squares.length; i++) {
     squares[i].addEventListener('click', updateGame);
   };
 };
-
-makeEventListeners();
 
 var isEmpty = function() {
   for (var i = 0; i < board.length; i++) {
@@ -49,6 +50,7 @@ var isEmpty = function() {
 var recordMove = function() {
   var target = event.target.id;
 
+  // rethink logic - the strings/IDs are arbitrary values in this context
   if (target === 'S1') {
     return board[0][0] = currentPlayer;
   };
@@ -85,7 +87,7 @@ var changeDOM = function() {
   };
   if (currentPlayer === player2) {
     event.target.textContent = 'O';
-    event.target.style.color = '#1F2593';
+    event.target.style.color = '#1f2593';
     event.target.style.backgroundColor = 'transparent';
   };
 };
@@ -99,18 +101,18 @@ var changePlayer = function() {
 };
 
 var checkWin = function() {
-  // to determine if a win condition has occured
-        // row wins
-  if (  ( (board[0][0] === board[0][1]) && (board[0][1] === board[0][2]) && (board[0][2] === currentPlayer) ) ||
-            ( (board[1][0] === board[1][1]) && (board[1][1] === board[1][2]) && (board[1][2] === currentPlayer) ) ||
-            ( (board[2][0] === board[2][1]) && (board[2][1] === board[2][2]) && (board[2][2] === currentPlayer) ) ||
-            // column wins
-            ( (board[0][0] === board[1][0]) && (board[1][0] === board[2][0]) && (board[2][0] === currentPlayer) ) ||
-            ( (board[0][1] === board[1][1]) && (board[1][1] === board[2][1]) && (board[2][1] === currentPlayer) ) ||
-            ( (board[0][2] === board[1][2]) && (board[1][2] === board[2][2]) && (board[2][2] === currentPlayer) ) ||
-            // diagonal wins
-            ( (board[0][0] === board[1][1]) && (board[1][1] === board[2][2]) && (board[2][2] === currentPlayer) ) ||
-            ( (board[0][2] === board[1][1]) && (board[1][1] === board[2][0]) && (board[2][0] === currentPlayer) ) ) {
+  // to determine if a win condition has occured - yikes!
+      // row wins
+  if (((board[0][0] === board[0][1]) && (board[0][1] === board[0][2]) && (board[0][2] === currentPlayer)) ||
+      ((board[1][0] === board[1][1]) && (board[1][1] === board[1][2]) && (board[1][2] === currentPlayer)) ||
+      ((board[2][0] === board[2][1]) && (board[2][1] === board[2][2]) && (board[2][2] === currentPlayer)) ||
+      // column wins
+      ((board[0][0] === board[1][0]) && (board[1][0] === board[2][0]) && (board[2][0] === currentPlayer)) ||
+      ((board[0][1] === board[1][1]) && (board[1][1] === board[2][1]) && (board[2][1] === currentPlayer)) ||
+      ((board[0][2] === board[1][2]) && (board[1][2] === board[2][2]) && (board[2][2] === currentPlayer)) ||
+      // diagonal wins
+      ((board[0][0] === board[1][1]) && (board[1][1] === board[2][2]) && (board[2][2] === currentPlayer)) ||
+      ((board[0][2] === board[1][1]) && (board[1][1] === board[2][0]) && (board[2][0] === currentPlayer))) {
       return winner = currentPlayer;
   } else {
       return false;
@@ -120,13 +122,13 @@ var checkWin = function() {
 var declareWinner = function() {
   if (winner === 'X') {
     player1Score += 1;
-    message.innerHTML = 'Player 1 Wins!';
+    message.textContent = 'Player 1 Wins!';
     document.getElementById('score-P1').textContent = player1Score;
   } else {
     player2Score += 1;
-    message.innerHTML = 'Player 2 Wins!';
+    message.textContent = 'Player 2 Wins!';
     document.getElementById('score-P2').textContent = player2Score;
-  }
+  };
 };
 
 var resetGame = function() {
@@ -135,46 +137,22 @@ var resetGame = function() {
       board[i][j] = 'E';
     };
   };
-  // clear all squares - DOM
+  // clears all squares and displays message
   setTimeout(function() {
-    message.textContent = 'Play again';
-    clearSquares}, 2000);
-  // play again - DOM
-
+    message.textContent = '●_●';
+    clearSquares();
+  }, 1000);
 };
 
 var clearSquares = function() {
   for (var i = 0; i < squares.length; i++) {
-    squares[i].innerHTML = '';
+    squares[i].textContent = '';
     squares[i].backgroundColor = '';
   };
 };
 
-
-
-var button = document.getElementById('button');
-button.addEventListener('click', clearSquares);
-
-// squares
-var s1 = document.getElementById('S1');
-var s2 = document.getElementById('S2');
-var s3 = document.getElementById('S3');
-var s4 = document.getElementById('S4');
-var s5 = document.getElementById('S5');
-var s6 = document.getElementById('S6');
-var s7 = document.getElementById('S7');
-var s8 = document.getElementById('S8');
-var s9 = document.getElementById('S9');
-
-var message = document.getElementById('message');
-
-// criteria
-// Design logic for winning & visually display which player won
-// DRY (Don't Repeat Yourself)
-// Use Javascript or jQuery for DOM manipulation - change classes
-// Deploy your game online
-
-// Deliverables
-// A link to your hosted working game in the URL section of your GitHub repo
-// A readme.md file with explanations of the technologies used, the approach taken,
-// installation instructions, unsolved problems, etc.
+button.addEventListener('click', function() {
+  makeEventListeners();
+  clearSquares();
+  button.textContent = 'Reset';
+});
