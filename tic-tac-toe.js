c7cb2b0ef825c3507a64cac16c67d37dcbac9c35
Duyen Ho <duyenho@gmail.com>
2016-10-16 14:12:59 +1100

    added draw condition

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,33 +5,41 @@ var button = document.getElementById('button');
 var player1 = 'X';
 var player2 = 'O';
 var currentPlayer = player1;
+var winner;
 
 var player1Score = 0;
 var player2Score = 0;
-var winner;
 
 var board = [['E', 'E', 'E'],
             ['E', 'E', 'E'],
             ['E', 'E', 'E']];
 
+var numberOfMoves = 0;
+var movesForDraw = board.length * board.length;
+
 var updateGame = function() {
 
   if (isEmpty()) {
-    recordMove();
-    changeDOM();
-    if (checkWin()) {
-      declareWinner();
-      resetGame();
-    } else {
-      changePlayer();
+    // will only run if square is empty
+    if (recordMove()) {
+      changeDOM();
+      // checks if current player has won
+      if (checkWin()) {
+        declareWinner();
+        resetGame();
+        // winner starts first in next game
+        changePlayer();
+      };
+        // checks for draw condition
+      if (numberOfMoves !== movesForDraw) {
+        // changes player for each turn
+        changePlayer();
+        return false;
+      } else {
+        message.textContent = 'It\'s a draw!';
+        resetGame();
+      };
     };
-  };
-
-};
-
-function makeEventListeners() {
-  for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('click', updateGame);
   };
 };
 
@@ -51,32 +59,73 @@ var recordMove = function() {
   var target = event.target.id;
 
   // rethink logic - the strings/IDs are arbitrary values in this context
-  if (target === 'S1') {
-    return board[0][0] = currentPlayer;
-  };
-  if (target === 'S2') {
-    return board[0][1] = currentPlayer;
-  };
-  if (target === 'S3') {
-    return board[0][2] = currentPlayer;
-  };
-  if (target === 'S4') {
-    return board[1][0] = currentPlayer;
-  };
-  if (target === 'S5') {
-    return board[1][1] = currentPlayer;
-  };
-  if (target === 'S6') {
-    return board[1][2] = currentPlayer;
-  };
-  if (target === 'S7') {
-    return board[2][0] = currentPlayer;
-  };
-  if (target === 'S8') {
-    return board[2][1] = currentPlayer;
-  };
-  if (target === 'S9') {
-    return board[2][2] = currentPlayer;
+  switch (target) {
+    case 'S1':
+      if (board[0][0] === 'E'){
+        board[0][0] = currentPlayer;
+      	numberOfMoves++;
+        return true;
+      };
+      break;
+    case 'S2':
+      if (board[0][1] === 'E'){
+        board[0][1] = currentPlayer;
+      	numberOfMoves++;
+        return true;
+      };
+      break;
+    case 'S3':
+      if (board[0][2] === 'E'){
+        board[0][2] = currentPlayer;
+        numberOfMoves++;
+        return true;
+      };
+      break;
+    case 'S4':
+      if (board[1][0] === 'E'){
+        board[1][0] = currentPlayer;
+        numberOfMoves++;
+        return true;
+      };
+      break;
+    case 'S5':
+      if (board[1][1] === 'E'){
+        board[1][1] = currentPlayer;
+      	numberOfMoves++;
+        return true;
+      };
+      break;
+    case 'S6':
+      if (board[1][2] === 'E'){
+        board[1][2] = currentPlayer;
+      	numberOfMoves++;
+        return true;
+      };
+      break;
+    case 'S7':
+      if (board[2][0] === 'E'){
+        board[2][0] = currentPlayer;
+      	numberOfMoves++;
+        return true;
+      };
+      break;
+    case 'S8':
+      if (board[2][1] === 'E'){
+        board[2][1] = currentPlayer;
+      	numberOfMoves++;
+        return true;
+      };
+      break;
+    case 'S9':
+      if (board[2][2] === 'E'){
+        board[2][2] = currentPlayer;
+      	numberOfMoves++;
+        return true;
+      };
+      break;
+    default:
+      return false;
+      break;
   };
 };
 
@@ -103,16 +152,16 @@ var changePlayer = function() {
 var checkWin = function() {
   // to determine if a win condition has occured - yikes!
       // row wins
-  if (((board[0][0] === board[0][1]) && (board[0][1] === board[0][2]) && (board[0][2] === currentPlayer)) ||
-      ((board[1][0] === board[1][1]) && (board[1][1] === board[1][2]) && (board[1][2] === currentPlayer)) ||
-      ((board[2][0] === board[2][1]) && (board[2][1] === board[2][2]) && (board[2][2] === currentPlayer)) ||
+  if (( (board[0][0] === board[0][1]) && (board[0][1] === board[0][2]) && (board[0][2] === currentPlayer) ) ||
+      ( (board[1][0] === board[1][1]) && (board[1][1] === board[1][2]) && (board[1][2] === currentPlayer) ) ||
+      ( (board[2][0] === board[2][1]) && (board[2][1] === board[2][2]) && (board[2][2] === currentPlayer) ) ||
       // column wins
-      ((board[0][0] === board[1][0]) && (board[1][0] === board[2][0]) && (board[2][0] === currentPlayer)) ||
-      ((board[0][1] === board[1][1]) && (board[1][1] === board[2][1]) && (board[2][1] === currentPlayer)) ||
-      ((board[0][2] === board[1][2]) && (board[1][2] === board[2][2]) && (board[2][2] === currentPlayer)) ||
+      ( (board[0][0] === board[1][0]) && (board[1][0] === board[2][0]) && (board[2][0] === currentPlayer) ) ||
+      ( (board[0][1] === board[1][1]) && (board[1][1] === board[2][1]) && (board[2][1] === currentPlayer) ) ||
+      ( (board[0][2] === board[1][2]) && (board[1][2] === board[2][2]) && (board[2][2] === currentPlayer) ) ||
       // diagonal wins
-      ((board[0][0] === board[1][1]) && (board[1][1] === board[2][2]) && (board[2][2] === currentPlayer)) ||
-      ((board[0][2] === board[1][1]) && (board[1][1] === board[2][0]) && (board[2][0] === currentPlayer))) {
+      ( (board[0][0] === board[1][1]) && (board[1][1] === board[2][2]) && (board[2][2] === currentPlayer) ) ||
+      ( (board[0][2] === board[1][1]) && (board[1][1] === board[2][0]) && (board[2][0] === currentPlayer) )) {
       return winner = currentPlayer;
   } else {
       return false;
@@ -122,11 +171,11 @@ var checkWin = function() {
 var declareWinner = function() {
   if (winner === 'X') {
     player1Score += 1;
-    message.textContent = 'Player 1 Wins!';
+    message.textContent = currentPlayer + ' wins!';
     document.getElementById('score-P1').textContent = player1Score;
   } else {
     player2Score += 1;
-    message.textContent = 'Player 2 Wins!';
+    message.textContent = currentPlayer + ' wins!';
     document.getElementById('score-P2').textContent = player2Score;
   };
 };
@@ -139,7 +188,8 @@ var resetGame = function() {
   };
   // clears all squares and displays message
   setTimeout(function() {
-    message.textContent = '●_●';
+    numberOfMoves = 0;
+    message.textContent = currentPlayer + ' starts';
     clearSquares();
   }, 1000);
 };
@@ -151,8 +201,14 @@ var clearSquares = function() {
   };
 };
 
+function makeEventListeners() {
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', updateGame);
+  };
+};
+
 button.addEventListener('click', function() {
   makeEventListeners();
-  clearSquares();
+  resetGame();
   button.textContent = 'Reset';
 });
